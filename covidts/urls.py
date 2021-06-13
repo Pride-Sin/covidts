76b02144aea8a1ad8b763886224467d135d3df32
Nicolas <nicolasre011@gmail.com>
2021-06-13 00:48:41 -0300

    Added URL includes to project urls

--- covidts/urls.py
+++ covidts/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 # Django imports
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 # Local imports
 from .views import Index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Index.as_view(), name='index'),
+    path('', include('users.urls')),
+    path('', include('pacients.urls')),
+    path('', include('ubications.urls')),
 ]
