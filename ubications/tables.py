d1fd1000105279b7f50ce3d9f99da02a80a51ad5
Nicolas <nicolasre011@gmail.com>
2021-06-18 18:28:36 -0300

    Delete button done, deleted ordering in actions rows

--- ubications/tables.py
+++ ubications/tables.py
@@ -1,9 +1,15 @@
-# Local imports
+# Tables2 imports
 import django_tables2 as tables
+from django_tables2.utils import A
+# Local imports
 from .models import Ubication
 
 
 class UbicationTable(tables.Table):
+    T1     = '''<a type="button" href="{% url 'delete-ubication' record.pk %}" class="btn btn-danger">Edit</a>'''
+    T2     = '''<a type="button" href="{% url 'delete-ubication' record.pk %}" class="btn btn-danger">Delete</a>'''
+    edit   = tables.TemplateColumn(T1, orderable=False)
+    delete = tables.TemplateColumn(T2, orderable=False)
     class Meta:
         model = Ubication
         fields = ("id", "city", "province", "poblation")
